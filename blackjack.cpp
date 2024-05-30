a6ab7418a29b9402edfc44da09f24a484c8aa7c3
izzatyi <nalizaizzati@gmail.com>
2024-05-30 21:25:22 +0700

    tambahan

--- blackjack.cpp
+++ blackjack.cpp
@@ -1,28 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <ctime>
-using namespace std;
+#include "card.h"
 
-class Card{
-    public:
-    
-    enum rank {
-        ACE = 1, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king
-    };
-    enum suit {
-        hearts, diamonds, clubs, spades
-    };
+Card :: Card(rank r, suit s, bool ifu) : m_Rank(r), m_Suit(s), m_IsFaceUp(ifu)
+{}
 
-    friend ostream& operator << (ostream& os, const Card& aCard);
-    Card(rank r = ACE, suit s = spades, bool ifu = true);
+int Card :: GetValue() const
+{
+    int value = 0;
+    if (m_IsFaceUp)
+    {
+        value = m_Rank;
+        if (value > 10)
+            value = 10;
+    }
+    return value;
+}
 
-    int GetValue() const;
-    void Flip();
-    private:
-    rank m_Rank;
-    suit m_Suit;
-    bool m_IsFaceUp;
-    
-};
+void Card :: Flip(){
+    m_IsFaceUp = !m_IsFaceUp;
+}
